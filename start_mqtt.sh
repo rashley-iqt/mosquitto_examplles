b62c3e73264c23b9ff6a933a2da2483e7174b0bc
Ryan <rashley@iqt.org>
2023-05-16 09:06:33 -0400

    adds cert authority container and bootstraps it

--- start_mqtt.sh
+++ start_mqtt.sh
@@ -5,6 +5,7 @@ touch /etc/mosquitto/passwd
 touch /var/log/mosquitto.log
 chown mosquitto:mosquitto /var/log/mosquitto.log
 chown mosquitto:mosquitto /etc/mosquitto/passwd
+# certbot
 mosquitto_passwd -b /etc/mosquitto/passwd $(cat /run/secrets/mqtt_user) $(cat /run/secrets/mqtt_pass)
 /usr/sbin/mosquitto -v -c /etc/mosquitto/mosquitto.conf
 
