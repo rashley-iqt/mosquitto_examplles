e081bfd4703c70659d6c391100b8dd2f30f42182
Ryan <rashley@iqt.org>
2023-05-31 10:31:23 -0400

    setup for renewal

--- server/start_mqtt.sh
+++ server/start_mqtt.sh
@@ -4,19 +4,22 @@ unset HISTFILE
 touch /etc/mosquitto/passwd
 touch /var/log/mosquitto.log
 chown mosquitto:mosquitto /var/log/mosquitto.log
-chown mosquitto:mosquitto /etc/mosquitto/passwd
+chown -R mosquitto:mosquitto /etc/mosquitto/
 ./trust_ca.sh
 certbot certonly --standalone --domains server.mqtt.local \
 --register-unsafely-without-email \
---server https://ca.mqtt.local:8050/acme/acme/directory
+--server https://ca.mqtt.local:8050/acme/acme/directory \
+--post-hook /update_mqtt_cfg.sh
 
 # by design the path for lets encrypt certs is inaccessible
 # copy certs to a location accessible by mosquitto and
 # adjust config accordingly
-mkdir /etc/mosquitto/certs
+[ ! -d "/etc/mosquitto/certs" ] && mkdir /etc/mosquitto/certs
 cp /etc/letsencrypt/live/server.mqtt.local/*.pem /etc/mosquitto/certs/
 chown -R mosquitto:mosquitto /etc/mosquitto/certs/
+
 mosquitto_passwd -b /etc/mosquitto/passwd $(cat /run/secrets/mqtt_user) $(cat /run/secrets/mqtt_pass)
+
 /usr/sbin/mosquitto -v -c /etc/mosquitto/mosquitto.conf
 
 # test with mosquitto_pub -h mqtt-server -u $(cat /run/secrets/mqtt_user) -P $(cat /run/secrets/mqtt_pass) -t "test" -m "Hats!"
